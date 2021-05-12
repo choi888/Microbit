7a0ff48d16b01fbc7bd9abb82ddb7a3408c487cb
choi888 <48800878+choi888@users.noreply.github.com>
2021-05-12 17:20:21 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,21 @@
 OLED12864_I2C.init(60)
 OLED12864_I2C.clear()
 basic.forever(function () {
-    for (let index = 0; index < 400; index++) {
-        OLED12864_I2C.showString(
-        1,
-        2,
-        "Hello World!",
-        1
-        )
-        basic.pause(5000)
-        OLED12864_I2C.clear()
-        OLED12864_I2C.rect(
-        0,
-        0,
-        60,
-        30,
-        1
-        )
-        basic.pause(5000)
-        OLED12864_I2C.clear()
-    }
+    OLED12864_I2C.showString(
+    1,
+    2,
+    "Hello World!",
+    1
+    )
+    basic.pause(5000)
+    OLED12864_I2C.clear()
+    OLED12864_I2C.rect(
+    0,
+    0,
+    60,
+    30,
+    1
+    )
+    basic.pause(5000)
+    OLED12864_I2C.clear()
 })
